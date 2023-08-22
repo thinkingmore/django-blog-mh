2d9a58a8ac72dec23d7b02b7358ae7633ce8cb50
thinkingmore <peterkevin823@gmail.comm>
2023-08-22 10:54:09 +0600

    added blog and blog related models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Category)
+admin.site.register(Tags)
+admin.site.register(Blog)
+admin.site.register(Comment)
+admin.site.register(Replies)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,90 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from accounts.models import User
+
+class Category(models.Model):
+    title = models.CharField(max_length=150,unique=True)
+    slug = models.SlugField(null=True)
+    created_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title 
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+class Tags(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(null=True)
+    created_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title  
+
+class Blog(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='user_blogs',
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        Category,
+        related_name='category_blogs',
+        on_delete=models.CASCADE
+    )
+    tags = models.ManyToManyField(
+        Tags,
+        related_name='tag_blogs',
+        blank=True
+    )
+    likes = models.ManyToManyField(
+        User,
+        related_name='user_likes',
+        blank=True
+    )
+    title = models.CharField(
+        max_length=250,
+    )
+    slug = models.SlugField(
+        null=True,
+        blank=True,
+    )
+    banner  = models.ImageField(upload_to="blog-baanners")
+    description = models.TextField()       
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='user_comments',
+        on_delete=models.CASCADE
+    )
+    blog = models.ForeignKey(
+        Blog,
+        related_name='blog_comments',
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+class Replies(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='user_replies',
+        on_delete=models.CASCADE
+    )
+    comment= models.ForeignKey(
+        Comment,
+        related_name='comment_replies',
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Replies'
