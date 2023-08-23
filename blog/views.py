61073baeb1b41bab678afa0051cee72246e134eb
thinkingmore <peterkevin823@gmail.comm>
2023-08-23 10:37:27 +0600

    rendered blogs page and added pagination

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from .models import Blog,Tags
 
 def index(request):
@@ -18,4 +18,22 @@ def contact(request):
     return render(request, 'contact.html')
 
 def blogs(request):
-    return render(request, 'blog.html')
+    queryset = Blog.objects.order_by('-created_date')
+    tags = Tags.objects.order_by('-created_date')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(queryset, 4)
+
+    try:
+        blogs = paginator.page(page)
+    except EmptyPage:
+        blogs = paginator.page(1)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+        return redirect('blogs')
+    
+    context = {
+        'blogs': blogs,
+        'tags': tags,
+        'pagination': paginator,
+    }
+    return render(request, 'blog.html', context=context)
